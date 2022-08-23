b6ba085a6f1243a0cb78d8a64ff0705602ed212f
aminulhaque98 <aminulhaque419@gmail.com>
2022-08-23 06:53:23 +0530

    budget option a alert set

--- js/utilites.js
+++ js/utilites.js
@@ -1,15 +1,17 @@
 function formHereInputValue(element) {
 
     const inputField = document.getElementById(element);
-
-    if (isNan(inputField) === true) {
-        alert('ato jamela kore keno')
-    }
-
     const inputFieldString = inputField.value;
     const inputValue = parseFloat(inputFieldString);
     inputField.value = '';
+
+    if (isNaN(inputValue) === true) {
+
+        alert('this number is a not valid number')
+
+    }
     return inputValue;
+
 };
 
 
